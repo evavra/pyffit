334bb21cff1ecad33271893fff2437501cadb656
ellisvavra <ellisvavra@berkeley.edu>
2022-11-01 12:02:25 -0700

    added basic data attributes and methods

--- pyffit/classes.py
+++ pyffit/classes.py
@@ -1,34 +1,38 @@
 import numpy as np
 import xarray as xr
+import datetime as dt
 
 
-class Interferogram(intf_path):
+class Interferogram:
     """
     Interferogram object designed around GMTSAR interferogram conventions.
     
     ATTRIBUTES:
-    intf_path - path to location of interferogram data files
-    
+    path - path to location of interferogram data files
+           Should follow GMTSAR interferogram naming convention:
+               pathYYYYMMDD_YYYYMMDD
+
     Will search for: 
+    amp       - SAR amplitude
     phase     - normal interferogram phase
     phasefilt - filtered interferogram phase
     corr      - interferogram coherence
     mask      - unwrapping mask
     landmask  - land/water mask
     unwrap    - unwrapped phase
-
     """
 
-    def __init__(self):
+    def __init__(self, path, verbose=True, **kwargs):
         # Initialize attributes
-        self.path      = intf_path
-        self.defaults  = ['phase',
+        self.path      = path
+        self.defaults  = ['amp',
+                          'phase',
                           'phasefilt',
                           'corr',
                           'mask',
-                          'landmask'
+                          'landmask',
                           'unwrap',]
-                        
+        # Spatial
         self.x         = None
         self.y         = None
         self.dims      = None
@@ -36,8 +40,19 @@ class Interferogram(intf_path):
         self.xmax      = None
         self.ymin      = None
         self.ymax      = None
+        self.xlim      = None
+        self.ylim      = None
         self.extent    = None
+        self.region    = None
+        self.track     = None
 
+        # Temporal
+        self.date_pair = None
+        self.dates     = None
+        self.days      = None
+
+        # InSAR
+        self.amp       = None
         self.phase     = None
         self.phasefilt = None
         self.corr      = None
@@ -45,26 +60,35 @@ class Interferogram(intf_path):
         self.unwrap    = None
         self.landmask  = None
 
-        self.epoch     = None
-        self.days      = None
+        # Load default attributes
+        self.load_track()
+        self.load_temporal_info()
+        self.load_data(files=self.defaults, verbose=verbose)
 
+        # Load additional attributes
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
 
-
-    def load_default_data(self, path):
+    def load_data(self, files=[], verbose=True):
         """
-        Attempt to load default interferogram attributes.
+        Attempt to load interferogram attributes.
         If files do not exist, attribute value will remain None.
+        Non-default files may be speficied with files argument.
         """
 
-        for file in self.defaults:
+        for data in self.defaults:
             try:
-                with xr.open_dataset(f'{path}/{file}.grd') as grd:
+                file_path = f'{self.path}/{data}.grd'
+
+                with xr.open_dataset(file_path) as grd:
                     try:
+                        # Try geographic coordinates
                         x = grd.lon.values
                         y = grd.lat.values
                         z = grd.z.values
 
                     except AttributeError:
+                        # Try radar coordinates
                         x = grd.x.values
                         y = grd.y.values
                         z = grd.z.values
@@ -78,10 +102,71 @@ class Interferogram(intf_path):
                 self.ymax   = np.nanmax(y)
                 self.xlim   = (self.xmin, self.xmax)
                 self.ylim   = (self.ymin, self.ymax)
-                self.extent = [self.xmin,
-                               self.xmax,
-                               self.ymin,
-                               self.ymax,]
+                self.dims   = z.shape
+                
+                if self.track == 'D':
+                    self.extent = [self.xmin,
+                                   self.xmax,
+                                   self.vmax,
+                                   self.ymin,]
+                else:
+                    self.extent = [self.xmin,
+                                   self.xmax,
+                                   self.ymin,
+                                   self.ymax,]
                 self.region = self.extent
-                               
-            except:
+
+                # Set data values
+                setattr(self, data, z)
+
+            except ValueError:
+                if verbose:
+                    print(f'Warning: {file_path} not found')
+        
+        return
+
+
+    def load_temporal_info(self,):
+        """
+        Extract datetime info from path.
+        """
+
+        # Date pair (directory name)
+        self.date_pair   = self.path.split('/')[-1]
+
+        # Datetime objects for dates
+        self.dates = [dt.datetime.strptime(date, '%Y%m%d') for date in self.date_pair.split('_')]
+
+        # Epoch length
+        self.days = (self.dates[1] - self.dates[0]).days
+
+        return
+
+
+    def load_track(self, verbose=True):
+        """
+        Attempt to identify satellite orbital track from filepath.
+        """
+
+        # Possible split directory strings, which track directory could be above
+        splits = ['F1', 'F2', 'F3', 'F4', 'F5', 'merge']
+
+        for split in splits:
+            track_strs = self.path.split(split) #[0].split('/')[-4]
+            print(track_strs)
+
+            if len(track_strs) > 1:
+                track = track_strs[0].split('/')[-2]
+
+                if any(key in track for key in ['A', 'ASC', ]):
+                    self.track = 'A'
+                    return
+
+                elif any(key in track for key in ['D', 'DES', ]):
+                    self.track = 'D'
+                    return
+
+                else:
+                    print('No satellite track identified.')
+                    return
+        return
