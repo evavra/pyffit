3ace967c88060ac5f7fdb27f278513299db7efa2
ellisvavra <ellisvavra@berkeley.edu>
2023-05-08 08:24:59 -0700

    moved fault trace reader to data

--- pyffit/data.py
+++ pyffit/data.py
@@ -127,3 +127,48 @@ def load_gmt_fault_file(file, region=[-180, 180, -90, 90], outname=''):
 
 
     return faults
+
+
+def read_traces(file, mode, EPSG='32611'):
+    
+    faults = {}
+
+    with open(file, 'r') as f:
+        # Load SCEC Community Fault Model file
+        if mode == 'SCEC':
+            for line in f:
+                if line[0] == '#':
+                    continue
+                if line[0] == '>':
+                    name = line.split()[-1][1:-1]
+                    faults[name] = ({'name': name, 
+                                      'lon': [], 
+                                      'lat': []})
+                else:
+                    coords = [float(item) for item in line.split()]
+                    faults[name]['lon'].append(coords[0])
+                    faults[name]['lat'].append(coords[1])
+
+        # Load digitized fault traces from QGIS .csv
+        elif mode == 'QGIS':
+            df = pd.read_csv(file, sep=',')
+            for name in df['Name'].unique():
+                df_fault = df[df['Name'] == name].sort_values('X')
+                faults[name] = {'Name': name, 
+                                'lon':  df_fault['X'].values, 
+                                'lat':  df_fault['Y'].values,}
+    
+
+    # Sort so that DataFrame is in descending latitude 
+    for name in faults:
+        fault = faults[name]
+        if fault['lat'][0] < fault['lat'][-1]:
+            fault['lon'] = fault['lon'][::-1]
+            fault['lat'] = fault['lat'][::-1]
+
+    # Add UTM coordinates
+    if EPSG:
+        for name in faults.keys():
+            faults[name]['UTMx'], faults[name]['UTMy'] = proj_ll2utm(faults[name]['lon'], faults[name]['lat'], EPSG)
+
+    return faults
