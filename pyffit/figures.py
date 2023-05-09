b32b296c2bde86153309a5fb1275142888b0bde9
ellisvavra <ellisvavra@berkeley.edu>
2023-05-09 10:25:17 -0700

    updated imports

--- pyffit/figures.py
+++ pyffit/figures.py
@@ -1,7 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import cmcrameri.cm as cmc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+from pyffit.data import read_traces
+from matplotlib.patches import Ellipse
 
 
 stem       = '/Users/evavra/Projects/Taiwan/ALOS2/A139/F4/'
