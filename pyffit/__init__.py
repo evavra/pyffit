9dac6285114a8b00c1589c16c51968bbcb25c38c
ellisvavra <ellisvavra@berkeley.edu>
2024-04-01 09:03:49 -0700

    properly formatted init file for importing

---
+++ pyffit/__init__.py
@@ -0,0 +1,11 @@
+from . import atmosphere
+from . import classes
+from . import corrections
+from . import data
+from . import figures
+from . import finite_fault
+from . import inversion
+# from . import profile_inversion
+from . import quadtree
+# from . import testing
+from . import utilities
