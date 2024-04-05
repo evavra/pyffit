a43ee274de6b3ec1b630311e19396d4c94b04bf0
ellisvavra <ellisvavra@berkeley.edu>
2024-04-05 07:07:22 -0700

    added decorrelation methods

--- pyffit/synthetics.py
+++ pyffit/synthetics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 def get_aps_params(klim=[-3, 5], kinc=0.1, amp_ref=5.0, nexp_ref=1.5, lc_ref=5):
@@ -15,19 +16,20 @@ def get_aps_params(klim=[-3, 5], kinc=0.1, amp_ref=5.0, nexp_ref=1.5, lc_ref=5):
     return amp, k, Pk
 
 
-def make_synthetic_aps(X0, Z0, klim=[-3, 5], kinc=0.1, amp_ref=5.0, nexp_ref=1.5, lc_ref=5):
+def make_synthetic_aps(X0, Z0, klim=[-3, 5], kinc=0.1, manual_amp=0, amp_ref=5.0, nexp_ref=1.5, lc_ref=5):
     """
     Generate "correlated noise" Ur(X,  Z) with a power spectrum Pk(k) starting with white noise,    
     then multipling its fft by Pk(k), going back in spce domain (ifft2)
     
     INPUT:
-    X0      - along strike coordinates (km)
-    Z0      - along dip coordinates (km)  
-    amp_ref - reference APS value
+    X0         - along strike coordinates (km)
+    Z0         - along dip coordinates (km)  
+    manual_amp - manually specify max/min value of noise
+    amp_ref    - reference APS value
     nexp_ref, lc_ref - power spectrum parameters
 
     OUTPUT:
-    Ursub
+    Ursub      - simulated atmospheric noise 
     """
 
     # Use larger grid dimension to start (will crop later)
@@ -62,9 +64,6 @@ def make_synthetic_aps(X0, Z0, klim=[-3, 5], kinc=0.1, amp_ref=5.0, nexp_ref=1.5
     K        = np.sqrt(KXX**2 + KZZ**2)                            # K in m^-1 
 
     # adjust the power-spectrum of the image Pk(k)
-    print(K.shape)
-    print(-Kny, Kny - (Kny*2)/N_max, (Kny*2)/N_max)
-
     aps_K  = np.fft.fftshift(np.fft.fft2(U))                         # direct Fourier transform
     aks_Kv = np.interp(K.flatten()*L, k,  Pk)                                 # fix |U(k)| = Pk(Kv)
     aps_K  = aks_Kv.reshape((N_max, N_max)) * np.exp(1j * np.angle(aps_K)) 
@@ -82,12 +81,20 @@ def make_synthetic_aps(X0, Z0, klim=[-3, 5], kinc=0.1, amp_ref=5.0, nexp_ref=1.5
     Ursub = Ur[:NZ0, :NX0]
 
     # Re-scale APS amplitude 
-    Ursub = Ursub/np.max(np.abs(Ursub)) * amp
+    if manual_amp > 0:
+        U_mean = np.mean(Ursub)                                          # Get mean
+        Ursub -= U_mean                                                  # Remove mean
+        Ursub  = (Ursub - np.min(Ursub))/(np.max(Ursub) - np.min(Ursub)) # Mormalize 0-1
+        Ursub  = 2 * manual_amp * Ursub - manual_amp                     # Rescale to specified amplitude
+        Ursub -= U_mean                                                  # Restore mean
 
+        # Ursub = Ursub/np.max(np.abs(Ursub)) * manual_amp
+
+    # print(U_mean, np.nanmin(Ursub), np.nanmax(Ursub))
     return Ursub
 
 
-def add_nans(data, nan_frac):
+def get_decorr_mask(data, nan_frac):
     """
     Introduce NaN values to data using uniform random distribution.
 
@@ -99,11 +106,86 @@ def add_nans(data, nan_frac):
     data (m, n) - updated data array with Nan values
     """
 
-    nans = random.sample(range(0, data.size), int(data.size*nan_frac))
+    mask = np.ones_like(data)
 
-    for i in nans:
-        j = i//data.shape[1]
-        k = i % data.shape[1]
-        data[j, k] = np.nan
+    if nan_frac > 0:
+        # Select NaN indices
+        nans = random.sample(range(0, data.size), int(data.size*nan_frac))
 
-    return data
+        # Apply NaN values
+        for i in nans:
+            j = i//data.shape[1]
+            k = i % data.shape[1]
+            mask[j, k] = np.nan
+
+    return mask
+
+
+def get_fault_mask(X, Y, trace, rupture_nan_frac=0.9, width=1):
+    """
+    Make synthetic mask for fault trace to simulate coseismic rupture decorrelation.
+    Assumes quasi-linear fault trace
+
+    INPUT:
+    X (m, n)     - x-coordinate array
+    Y (m, n)     - y-coordinate array
+    trace (p, 2) - x/y coordinates of fault trace
+    l_mask       - distance (km) 
+    """ 
+
+    # Calculate distances
+    dists = np.empty_like(X)
+
+    for i in range(len(X)):
+        for j in range(len(X[0])):
+            dists[i, j] = get_line_segment_distance(trace, (X[i, j], Y[i, j]))
+
+
+    # Apply Gaussian kernel to get Nan probabilities
+    gauss = (1/(width * np.sqrt(2*np.pi))) * np.exp(-0.5 * (dists/width)**2)
+    gauss *= rupture_nan_frac/gauss.max()
+
+    # Evaluate probabilities
+    mask = np.ones_like(X)
+    probs = np.random.uniform(low=0, high=1, size=X.shape)
+    mask[probs < gauss] = np.nan
+
+    return mask
+
+
+def get_line_segment_distance(segment, point):
+    """
+    Calculate min. distance ebtween point and line segment.
+
+    INPUT:
+    segment (m, 2) - 
+    point   (2,)   - 
+
+    OUTPUT:
+    dist           - 
+    """
+
+    x_min = segment[0, 0]
+    x_max = segment[-1, 0]
+    y_min = segment[0, 1]
+    y_max = segment[-1, 1]
+
+    run  = x_max - x_min
+    rise = y_max - y_min
+
+    u =  ((point[0] - x_min) * run + (point[1] - y_min) * rise) / (run**2 + rise**2)
+
+    if u > 1:
+        u = 1
+    elif u < 0:
+        u = 0
+
+    x = x_min + u * run
+    y = y_min + u * rise
+
+    dx = x - point[0]
+    dy = y - point[1]
+
+    dist = np.sqrt(dx**2 + dy**2)
+
+    return dist
