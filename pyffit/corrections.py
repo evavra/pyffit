d580e5a84f30837c92cfed4b4a711ddf6e6cc5b3
ellisvavra <ellisvavra@berkeley.edu>
2024-04-22 08:40:30 -0700

    Updated ramp removal method

--- pyffit/corrections.py
+++ pyffit/corrections.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def fit_ramp(x, y, z, deg=1, region=[], grid=True):
+def fit_ramp(x, y, z, x_ramp=[], y_ramp=[], deg=1, region=[-360, 360, -90, 90], grid=True):
     """
     Fit 2D polynomnial ramp to data.
 
@@ -19,10 +19,10 @@ def fit_ramp(x, y, z, deg=1, region=[], grid=True):
     ramp - modeled ramp (m, n) or (m*n_)
     """
 
-    # Handle coordinates
-    if (x.shape == z.shape) & (y.shape == z.shape): # Pre-flattened
+    # Handle coordinates    
+    if (x.shape == z.shape) & (y.shape == z.shape) & (len(z.shape) == 1): # Pre-flattened
         pass
-    elif (x.shape == y.shape) & len(x.shape) == 2: # Fully gridded 
+    elif (x.shape == y.shape) & (len(x.shape) == 2): # Fully gridded 
         x = x.flatten()
         y = y.flatten()
     elif (x.shape != y.shape) & len(x.shape) == 1: # Grid axes only
@@ -41,40 +41,39 @@ def fit_ramp(x, y, z, deg=1, region=[], grid=True):
     if len(region) == 4:
         region_mask = (x >= region[0]) & (x <= region[1]) & (y >= region[2]) & (y <= region[3])
 
-    # Normalize 
+    # Normalize to void numerical errors
     x  = x - x.min()
     x /= x.max()
     y  = y - y.min()
     y /= y.max()
 
+    if (len(x_ramp) == 0) & (len(y_ramp) == 0):
+        x_ramp = x
+        y_ramp = y
+
     # Set up linear equations
     if deg == 1:
-        G_grid = np.vstack((x, y, np.ones_like(x))).T
-        # G_grid = np.polynomial.polynomial.polyvander2d(x, y, [deg, deg]) # Full vandemond matrix
+        get_G = lambda x, y: np.vstack((x, y, np.ones_like(x))).T
 
     elif deg == 2:
-        # G_grid = np.vstack((x**2, x*y, y**2, x, y, x**0)).T
-        # G_grid = np.vstack((x**2, y**2, x*y, x, y, np.ones_like(x))).T
-        # G_grid = np.vstack((x**2, y**2, x, y, np.ones_like(x))).T
-        G_grid = np.array([x**2, np.ones_like(x), x, y, (x**2)*y, (x**2)*(y**2), y**2, x*(y**2), x*y]).T
+        get_G = lambda x, y: np.array([x**2, np.ones_like(x), x, y, (x**2)*y, (x**2)*(y**2), y**2, x*(y**2), x*y]).T
 
-        # G_grid = np.polynomial.polynomial.polyvander2d(x, y, [deg, deg]) # Full vandemond matrix
+    # Get design matrices
+    G_fit  = get_G(x, y)
+    G_ramp = get_G(x_ramp, y_ramp)
 
     # Omit nans for least-squares solve
     nans = np.isnan(d)
-    G    = G_grid[(~nans) & region_mask, :]
+    G    = G_fit[(~nans) & region_mask, :]
     d    = d[(~nans) & region_mask]
-
-    print(G_grid.shape, d.shape)
-    print(G.shape, d.shape)
-    print('Percent NaN', 100*sum(nans)/len(nans))
+    # print(f'Percent NaN: {100*sum(nans)/len(nans):.1f} %')
 
     # Solve for ramp coefficients
     m, residuals, rank, s = np.linalg.lstsq(G, d, rcond=None)
 
-    # Return ramp at all grid points
+    # Return ramp at specified points
     if grid & (len(z.shape) == 2):
-        d_grid = G_grid @ m
+        d_grid = G_ramp @ m
         return d_grid.reshape(z.shape)
 
     # Return grid at data locations only
