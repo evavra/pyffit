b34995134af2a2d377b11525bc0821d4e5174f35
ellisvavra <ellisvavra@berkeley.edu>
2025-03-25 12:26:49 -0700

    added covariance estimation parameters

--- tests/NIF/data_params.py
+++ tests/NIF/data_params.py
@@ -3,15 +3,15 @@
 # This allows for easier specification of variables with float, array-like, and other non-string types (i.e. colormaps)
 
 # IMPORTANT: the order of the parameters matters! The required parameters are positional arguments to the relevant
-# driver method in nif.py taht corresponds to 'mode'. Thus, they should always be specified first and in the same 
+# driver method in nif.py that corresponds to 'mode'. Thus, they should always be specified first and in the same 
 # order as listed in the driver. However, the order of the remaining named parameters can be arbitrary.
 
 # -------------------------- Imports --------------------------
 import cmcrameri.cm as cmc
 
 # -------------------------- Required parameters --------------------------
-# Run mode
-mode = 'NIF' # NIF for standard NIF run              
+# Run mode(s)
+mode = ['analyze'] # NIF for standard NIF run              
 
 # Files and directories
 mesh_file           = '/Users/evavra/Projects/SSAF/Analysis/Finite_Fault_Modeling/Mesh/Geometry/mesh_points.txt'
@@ -35,12 +35,25 @@ reference_time_series = True  # Reference whole time series to the observation a
 use_dates             = True  # True formatted dates ()'YYYY-MM-DD' or DateTime objects), False to use raw str specified in date_index_range.
 use_datetime          = True  # Convert dates from str to DateTime
 dt                    = 12    # Epoch length (days)
+data_factor           = 10    # Convert from cm to desired units
 
 # Files
 # model_file          = '/Users/evavra/Software/pyffit/tests/NIF/synthetic_data_full/slip_model.h5'
 # mask_file           = '/Users/evavra/Software/pyffit/tests/NIF/synthetic_data_full/decorr_mask.grd'
 model_file = ''
 mask_file  = ''
+mask_dir   = '/Users/evavra/Projects/SSAF/Analysis/Time_Series/NIF/masks/'
+cov_dir    = '/Users/evavra/Projects/SSAF/Analysis/Time_Series/NIF/covariance/'
+
+# Covariance estimation parameters
+estimate_covariance = True
+mask_dists          = [3]
+n_samp              = 2*10**7
+r_inc               = 0.2
+r_max               = 80
+m0                  = [0.9, 1.5, 5]
+c_max               = 2
+sv_max              = 2
 
 # Geographic parameters
 # EPSG        = '32611' 
@@ -50,19 +63,19 @@ avg_strike  = 315.8
 trace_inc   = 0.01
 
 # Fault parameters
-poisson_ratio = 0.25      # Poisson ratio
-shear_modulus = 6 * 10**9 # Shear modulus (Pa)
-disp_components = [1]     # displacement components to use [0 for fault-perpendicular, 1 for fault-parallel, 2 for vertical]
-slip_components = [0]     # slip components to use [0 for strike-slip, 1 for dip-slip, 2 for opening]
+poisson_ratio   = 0.25      # Poisson ratio
+shear_modulus   = 6 * 10**9 # Shear modulus (Pa)
+disp_components = [1]       # displacement components to use [0 for fault-perpendicular, 1 for fault-parallel, 2 for vertical]
+slip_components = [0]       # slip components to use [0 for strike-slip, 1 for dip-slip, 2 for opening]
 
 # Resolution based resampling
 # resolution_threshold = 2.3e-1 # cutoff value for resolution matrix (lower values = more points)
 resolution_threshold = 1.0 # cutoff value for resolution matrix (lower values = more points)
-width_min            = 0.1    # Min. allowed cell size (km)
-width_max            = 10     # Max. allowed cell size (km)
-max_intersect_width  = 100    # Max. allowed size for fault-intersecting cells (km)
-min_fault_dist       = 1      # Buffer distance from fault to enforce max_intersect width
-max_iter             = 10     # Max. allowed sampling iterations
+width_min            = 0.1 # Min. allowed cell size (km)
+width_max            = 10  # Max. allowed cell size (km)
+max_intersect_width  = 100 # Max. allowed size for fault-intersecting cells (km)
+min_fault_dist       = 1   # Buffer distance from fault to enforce max_intersect width
+max_iter             = 10  # Max. allowed sampling iterations
 smoothing_samp       = False
 edge_slip_samp       = False
 
@@ -87,10 +100,10 @@ W_dot_lim       = (0, 100) # min./max. bounds on transient rate slip values (mm/
 # Plot parameters
 xlim       = [-35.77475071, 26.75029172]
 ylim       = [-26.75029172, 55.08597388]
-vlim_slip  = [0, 4]
-vlim_disp  = [[-2, 2],
-              [-2, 2],
-              [-2, 2]] 
+vlim_slip  = [0, 40]
+vlim_disp  = [[-20, 20],
+              [-20, 20],
+              [-20, 20]] 
 cmap_disp  = cmc.vik
 figsize    = (10, 7)
 dpi        = 75
